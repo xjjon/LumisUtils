e89471f01013a8fa41b0a7fa8b6f6822342431de
= <oninuva@gmail.com>
2018-04-15 21:39:10 -0700

    Cooldown Timer and ChainedActionHandler Unit Tests

---
+++ Assets/Editor/Tests/Actions/ChainedActionHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.Actions;
+using Moq;
+using NUnit.Framework;
+
+[TestFixture]
+public class ChainedActionHandlerTest
+{
+    private ChainedActionHandler _chainedActionHandler;
+
+    private Mock<IActionHandler> _mockedActionHandler1;
+    private Mock<IActionHandler> _mockedActionHandler2;
+
+    [Test]
+    public void ExecuteFailOnFirstActionTest()
+    {
+        _mockedActionHandler1 = MockActionHandler(false);
+        _mockedActionHandler2 = MockActionHandler(true);
+        _chainedActionHandler = new ChainedActionHandler(
+            new List<IActionHandler>(
+                new[] { _mockedActionHandler1.Object, _mockedActionHandler2.Object }));
+
+        var result = _chainedActionHandler.Execute();
+
+        Assert.IsFalse(result);
+        _mockedActionHandler1.Verify(x => x.Execute(), Times.Exactly(1));
+        _mockedActionHandler2.Verify(x => x.Execute(), Times.Never);
+    }
+
+    [Test]
+    public void ExecuteSuceedAllActionsTest()
+    {
+        _mockedActionHandler1 = MockActionHandler(true);
+        _mockedActionHandler2 = MockActionHandler(true);
+        _chainedActionHandler = new ChainedActionHandler(
+            new List<IActionHandler>(
+                new[] { _mockedActionHandler1.Object, _mockedActionHandler2.Object }));
+
+        var result = _chainedActionHandler.Execute();
+
+        Assert.IsTrue(result);
+        _mockedActionHandler1.Verify(x => x.Execute(), Times.Exactly(1));
+        _mockedActionHandler2.Verify(x => x.Execute(), Times.Exactly(1));
+    }
+
+    private Mock<IActionHandler> MockActionHandler(bool state)
+    {
+        var actionHandler = new Mock<IActionHandler>();
+        actionHandler.Setup(x => x.Execute()).Returns(state);
+        return actionHandler;
+    }
+}
---
+++ Assets/Editor/Tests/Timers/CooldownTimerTest.cs
@@ -0,0 +1,138 @@
+﻿using Assets.Scripts.Timers;
+using NUnit.Framework;
+
+[TestFixture]
+public class CooldownTimerTest
+{
+
+    private CooldownTimer _cooldownTimer;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _cooldownTimer = new CooldownTimer(1, false);
+        _cooldownTimer.TimerCompleteEvent += () => { Assert.IsTrue(_cooldownTimer.Completed); };
+    }
+
+    [Test]
+    public void NormalNonRecurringTest()
+    {
+        Assert.IsFalse(_cooldownTimer.IsActive);
+        Assert.IsFalse(_cooldownTimer.IsReccuring);
+        Assert.IsFalse(_cooldownTimer.Completed);
+        Assert.AreEqual(1, _cooldownTimer.TimeRemaining);
+        Assert.AreEqual(1, _cooldownTimer.TotalTime);
+        Assert.AreEqual(0, _cooldownTimer.TimeElapsed);
+        Assert.AreEqual(0, _cooldownTimer.TimesCounted);
+
+        _cooldownTimer.Start();
+
+        Assert.IsTrue(_cooldownTimer.IsActive);
+        Assert.IsFalse(_cooldownTimer.IsReccuring);
+        Assert.IsFalse(_cooldownTimer.Completed);
+        Assert.AreEqual(1, _cooldownTimer.TimeRemaining);
+        Assert.AreEqual(1, _cooldownTimer.TotalTime);
+        Assert.AreEqual(0, _cooldownTimer.TimeElapsed);
+        Assert.AreEqual(0, _cooldownTimer.TimesCounted);
+
+        _cooldownTimer.Update(1);
+
+        Assert.IsFalse(_cooldownTimer.IsActive);
+        Assert.IsFalse(_cooldownTimer.IsReccuring);
+        Assert.IsTrue(_cooldownTimer.Completed);
+        Assert.AreEqual(0, _cooldownTimer.TimeRemaining);
+        Assert.AreEqual(1, _cooldownTimer.TotalTime);
+        Assert.AreEqual(1, _cooldownTimer.TimeElapsed);
+        Assert.AreEqual(1, _cooldownTimer.TimesCounted);
+
+        _cooldownTimer.Update(1);
+
+        Assert.IsFalse(_cooldownTimer.IsActive);
+        Assert.IsFalse(_cooldownTimer.IsReccuring);
+        Assert.IsTrue(_cooldownTimer.Completed);
+        Assert.AreEqual(0, _cooldownTimer.TimeRemaining);
+        Assert.AreEqual(1, _cooldownTimer.TotalTime);
+        Assert.AreEqual(1, _cooldownTimer.TimeElapsed);
+        Assert.AreEqual(1, _cooldownTimer.TimesCounted);
+    }
+
+    [Test]
+    public void NormalRecurringTest()
+    {
+        _cooldownTimer = new CooldownTimer(1, true);
+
+        Assert.IsFalse(_cooldownTimer.IsActive);
+        Assert.IsTrue(_cooldownTimer.IsReccuring);
+        Assert.IsFalse(_cooldownTimer.Completed);
+        Assert.AreEqual(1, _cooldownTimer.TimeRemaining);
+        Assert.AreEqual(1, _cooldownTimer.TotalTime);
+        Assert.AreEqual(0, _cooldownTimer.TimeElapsed);
+        Assert.AreEqual(0, _cooldownTimer.TimesCounted);
+
+        _cooldownTimer.Start();
+
+        Assert.IsTrue(_cooldownTimer.IsActive);
+        Assert.IsTrue(_cooldownTimer.IsReccuring);
+        Assert.IsFalse(_cooldownTimer.Completed);
+        Assert.AreEqual(1, _cooldownTimer.TimeRemaining);
+        Assert.AreEqual(1, _cooldownTimer.TotalTime);
+        Assert.AreEqual(0, _cooldownTimer.TimeElapsed);
+        Assert.AreEqual(0, _cooldownTimer.TimesCounted);
+
+        _cooldownTimer.Update(1);
+
+        Assert.IsTrue(_cooldownTimer.IsActive);
+        Assert.IsTrue(_cooldownTimer.IsReccuring);
+        Assert.IsFalse(_cooldownTimer.Completed);
+        Assert.AreEqual(1, _cooldownTimer.TimeRemaining);
+        Assert.AreEqual(1, _cooldownTimer.TotalTime);
+        Assert.AreEqual(0, _cooldownTimer.TimeElapsed);
+        Assert.AreEqual(1, _cooldownTimer.TimesCounted);
+
+        _cooldownTimer.Update(0.5f);
+
+        Assert.IsTrue(_cooldownTimer.IsActive);
+        Assert.IsTrue(_cooldownTimer.IsReccuring);
+        Assert.IsFalse(_cooldownTimer.Completed);
+        Assert.AreEqual(0.5f, _cooldownTimer.TimeRemaining);
+        Assert.AreEqual(1, _cooldownTimer.TotalTime);
+        Assert.AreEqual(0.5f, _cooldownTimer.TimeElapsed);
+        Assert.AreEqual(1, _cooldownTimer.TimesCounted);
+    }
+
+    [Test]
+    public void AddTimeTest()
+    {
+        _cooldownTimer.Start();
+        _cooldownTimer.Update(0.5f);
+
+        Assert.IsTrue(_cooldownTimer.IsActive);
+        Assert.IsFalse(_cooldownTimer.IsReccuring);
+        Assert.IsFalse(_cooldownTimer.Completed);
+        Assert.AreEqual(0.5f, _cooldownTimer.TimeRemaining);
+        Assert.AreEqual(1, _cooldownTimer.TotalTime);
+        Assert.AreEqual(0.5f, _cooldownTimer.TimeElapsed);
+        Assert.AreEqual(0, _cooldownTimer.TimesCounted);
+
+        _cooldownTimer.AddTime(0.5f);
+
+        Assert.IsTrue(_cooldownTimer.IsActive);
+        Assert.IsFalse(_cooldownTimer.IsReccuring);
+        Assert.IsFalse(_cooldownTimer.Completed);
+        Assert.AreEqual(1, _cooldownTimer.TimeRemaining);
+        Assert.AreEqual(1.5f, _cooldownTimer.TotalTime);
+        Assert.AreEqual(0.5f, _cooldownTimer.TimeElapsed);
+        Assert.AreEqual(0, _cooldownTimer.TimesCounted);
+
+        _cooldownTimer.Update(1);
+
+        Assert.IsFalse(_cooldownTimer.IsActive);
+        Assert.IsFalse(_cooldownTimer.IsReccuring);
+        Assert.IsTrue(_cooldownTimer.Completed);
+        Assert.AreEqual(0, _cooldownTimer.TimeRemaining);
+        Assert.AreEqual(1.5f, _cooldownTimer.TotalTime);
+        Assert.AreEqual(1.5f, _cooldownTimer.TimeElapsed);
+        Assert.AreEqual(1, _cooldownTimer.TimesCounted);
+    }
+
+}
